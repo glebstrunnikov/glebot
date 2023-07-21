d3f6f4697b7e0a7e78967b9a1dbd233a1143fdd7
Gleb Strunnikov <glebstrunnikov@gmail.com>
2023-07-21 18:59:08 +0300

    added weather by gps

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const host = process.env.BOT_HOST;
 const user = process.env.BOT_USER;
 const password = process.env.BOT_PASSWORD;
 const database = process.env.BOT_DATABASE;
+const weatherApiKey = process.env.WEATHER_API_KEY;
 const bot = new TelegramApi(token, { polling: true });
+const fetch = require("node-fetch");
 const on_message = require("./modules/on_message.js");
 const on_callback_query = require("./modules/on_callback_query.js");
 
@@ -31,7 +33,7 @@ async function asyncConnection() {
     fs.appendFileSync(path.join(__dirname, "log.txt"), "sending query \n");
     const res = await conn.query("DESCRIBE tasks;");
     fs.appendFileSync(path.join(__dirname, "log.txt"), "query sent \n");
-    console.log(res);
+    // console.log(res);
     fs.appendFileSync(
       path.join(__dirname, "log.txt"),
       `response: ${JSON.stringify(res)} \n`
@@ -71,7 +73,7 @@ async function asyncConnection() {
 
     let mode = "default";
 
-    bot.on("message", async (msg) => {
+    bot.on("text", async (msg) => {
       mode = await on_message(msg, bot, conn, mode, toDoBtns, toDoDisplay);
     });
 
@@ -84,6 +86,55 @@ async function asyncConnection() {
         toDoDisplay,
         toDoBtns
       );
+    });
+
+    bot.on("location", async (msg) => {
+      let currentWeather = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${msg.location.latitude}&lon=${msg.location.longitude}&appid=${weatherApiKey}&units=metric&lang=ru`
+      );
+      currentWeather = await currentWeather.json();
+      let tomorrowWeather = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${msg.location.latitude}&lon=${msg.location.longitude}&appid=${weatherApiKey}&units=metric&cnt=8&lang=ru`
+      );
+      tomorrowWeather = await tomorrowWeather.json();
+
+      let listOfWeatherConditions = [];
+      tomorrowWeather.list.forEach((threeHourlyForecast) => {
+        threeHourlyForecast.weather.forEach((weatherObject) => {
+          if (
+            listOfWeatherConditions.indexOf(weatherObject.description) === -1
+          ) {
+            listOfWeatherConditions.push(weatherObject.description);
+          }
+        });
+      });
+
+      let tomorrowTempMax;
+      let tomorrowTempMin;
+      tomorrowWeather.list.forEach((threeHourlyForecast) => {
+        if (!tomorrowTempMax) {
+          tomorrowTempMax = threeHourlyForecast.main.temp;
+        }
+        if (!tomorrowTempMin) {
+          tomorrowTempMin = threeHourlyForecast.main.temp;
+        }
+        if (threeHourlyForecast.main.temp > tomorrowTempMax) {
+          tomorrowTempMax = threeHourlyForecast.main.temp;
+        }
+        if (threeHourlyForecast.main.temp < tomorrowTempMin) {
+          tomorrowTempMin = threeHourlyForecast.main.temp;
+        }
+      });
+      console.log(listOfWeatherConditions);
+      console.log("max: " + tomorrowTempMax + ", min: " + tomorrowTempMin);
+      const weatherReport = `Температура сейчас: ${
+        Math.floor(currentWeather.main.temp * 10) / 10
+      }, ${currentWeather.weather[0].description} (ощущается как ${
+        Math.floor(currentWeather.main.feels_like * 10) / 10
+      })\n\nВ ближайшие сутки: от ${Math.floor(tomorrowTempMin * 10) / 10} до ${
+        Math.floor(tomorrowTempMax * 10) / 10
+      }, ${listOfWeatherConditions.join(", ")}`;
+      bot.sendMessage(msg.chat.id, weatherReport);
     });
   } catch (error) {
     fs.appendFileSync(path.join(__dirname, "log.txt"), JSON.stringify(error));
