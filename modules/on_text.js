c8037060258792021bc7d996ffa469a0c69d1952
Gleb Strunnikov <glebstrunnikov@gmail.com>
2023-07-28 02:17:28 +0300

    refactored on_text from ifs to switch

--- modules/on_text.js
+++ modules/on_text.js
@@ -2,117 +2,116 @@ const onText = async (msg, bot, conn, mode, toDoBtns, displayToDos, openai) => {
   const text = msg.text;
   const chat = msg.chat.id;
   const userId = msg.chat.id;
-  if (!mode[chat]) {
+  if (text === "/start") {
+    bot.sendMessage(chat, "Privet", toDoBtns);
     mode[chat] = "default";
+    return mode;
   }
 
   let toDoList = await conn.query(
     `SELECT * FROM tasks WHERE user_id=${userId}`
   );
 
-  if (text === "/start") {
-    bot.sendMessage(chat, "Privet", toDoBtns);
-    mode[chat] = "default";
-    return mode;
-  }
-  if (mode[chat] === "toDoAddingItem") {
-    toDoList.push({ text: text });
-    conn.query(
-      `INSERT INTO tasks (user_id, text) VALUES('${userId}', '${text}')`
-    );
-    bot.sendMessage(
-      chat,
-      `Кайф, новый список дел:\n\n${displayToDos(toDoList)}`,
-      toDoBtns
-    );
-    return mode;
-  }
-
-  if (mode[chat] === "toDoDeletingItem") {
-    if (
-      Number(text) <= toDoList.length &&
-      Number(text) > 0 &&
-      Number(text) === Math.floor(Number(text))
-    ) {
-      const toDoToDelete = toDoList[text - 1];
-      conn.query(`DELETE FROM tasks WHERE id='${toDoToDelete.id}'`);
+  switch (mode[chat]) {
+    case undefined:
       mode[chat] = "default";
-      toDoList.splice(text - 1, 1);
+    // falls through
+    case "toDoAddingItem":
+      toDoList.push({ text: text });
+      conn.query(
+        `INSERT INTO tasks (user_id, text) VALUES('${userId}', '${text}')`
+      );
       bot.sendMessage(
         chat,
         `Кайф, новый список дел:\n\n${displayToDos(toDoList)}`,
         toDoBtns
       );
-      return mode;
-    } else {
-      bot.sendMessage(
-        chat,
-        `Так не пойдет, пришли число больше 0 и меньше ${
-          toDoList.length + 1
-        }\n\n${displayToDos(toDoList)}`
+      break;
+    case "toDoDeletingItem":
+      if (
+        Number(text) <= toDoList.length &&
+        Number(text) > 0 &&
+        Number(text) === Math.floor(Number(text))
+      ) {
+        const toDoToDelete = toDoList[text - 1];
+        conn.query(`DELETE FROM tasks WHERE id='${toDoToDelete.id}'`);
+        mode[chat] = "default";
+        toDoList.splice(text - 1, 1);
+        bot.sendMessage(
+          chat,
+          `Кайф, новый список дел:\n\n${displayToDos(toDoList)}`,
+          toDoBtns
+        );
+        break;
+      } else {
+        bot.sendMessage(
+          chat,
+          `Так не пойдет, пришли число больше 0 и меньше ${
+            toDoList.length + 1
+          }\n\n${displayToDos(toDoList)}`
+        );
+        break;
+      }
+    case "toDoEditingItem": {
+      if (!/^\d+\s.+$/.test(text)) {
+        bot.sendMessage(
+          chat,
+          "Ты что-то напутал с форматом. Пришли номер дела, пробел и новую редакцию"
+        );
+        break;
+      }
+      const editText = text.replaceAll(/^.+?\s/g, "").trim();
+      const editNo = text.replaceAll(/\s.*$/g, "").trim();
+      if (Number(editNo) === 0) {
+        bot.sendMessage(
+          chat,
+          "Номер дела не может быть равен нулю. Попробуй еще раз"
+        );
+        break;
+      }
+      if (editNo >= toDoList.length + 1) {
+        bot.sendMessage(chat, "У тебя нет столько дел в списке");
+        break;
+      }
+      conn.query(
+        `UPDATE tasks SET text = '${editText}' WHERE id = ${
+          toDoList[editNo - 1].id
+        }`
       );
-      return mode;
-    }
-  }
-
-  if (mode[chat] === "toDoEditingItem") {
-    if (!/^\d+\s.+$/.test(text)) {
+      toDoList[editNo - 1].text = editText;
       bot.sendMessage(
         chat,
-        "Ты что-то напутал с форматом. Пришли номер дела, пробел и новую редакцию"
-      );
-      return mode;
-    }
-    const editText = text.replaceAll(/^.+?\s/g, "").trim();
-    const editNo = text.replaceAll(/\s.*$/g, "").trim();
-    if (Number(editNo) === 0) {
-      bot.sendMessage(
-        chat,
-        "Номер дела не может быть равен нулю. Попробуй еще раз"
-      );
-      return mode;
-    }
-    if (editNo >= toDoList.length + 1) {
-      bot.sendMessage(chat, "У тебя нет столько дел в списке");
-      return mode;
-    }
-    conn.query(
-      `UPDATE tasks SET text = '${editText}' WHERE id = ${
-        toDoList[editNo - 1].id
-      }`
-    );
-    toDoList[editNo - 1].text = editText;
-    bot.sendMessage(
-      chat,
-      `Кайф, новый список дел:\n\n${displayToDos(toDoList)}`,
-      toDoBtns
-    );
-    mode[chat] = "default";
-    return mode;
-  }
-
-  if (mode[chat] === "writeChatGpt") {
-    if (text.trim() === "/exit") {
-      bot.sendMessage(
-        chat,
-        "Окей, ты больше не разговариваешь с ChatGPT",
+        `Кайф, новый список дел:\n\n${displayToDos(toDoList)}`,
         toDoBtns
       );
       mode[chat] = "default";
-      return mode;
-    } else {
-      const chatCompletion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: text }],
-      });
-      await bot.sendMessage(
-        chat,
-        chatCompletion.data.choices[0].message.content
-      );
-      return mode;
+      break;
     }
+    case "writeChatGpt":
+      if (text.trim() === "/exit") {
+        bot.sendMessage(
+          chat,
+          "Окей, ты больше не разговариваешь с ChatGPT",
+          toDoBtns
+        );
+        mode[chat] = "default";
+        break;
+      } else {
+        const chatCompletion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: text }],
+        });
+        await bot.sendMessage(
+          chat,
+          chatCompletion.data.choices[0].message.content
+        );
+        break;
+      }
+    default:
+      bot.sendMessage(chat, "Не понял");
+      break;
   }
-  bot.sendMessage(chat, "Не понял");
+
   return mode;
 };
 
